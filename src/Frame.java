b0387be3fbedd0b3eab91413181f1a4aad0da731
Vaibav <vaibavsubbu007@gmail.com>
2023-03-21 07:36:24 -0500

    frame

--- src/Frame.java
+++ src/Frame.java
@@ -5,6 +5,8 @@ public class Frame extends JFrame {
     public Frame(int width, int height) {
         JFrame frame = new JFrame("Kingdom Builder");
         frame.setSize(width, height);
-
+        frame.setVisible(true);
+        frame.setResizable(false);
+        add(new JPanel());
     }
 }
