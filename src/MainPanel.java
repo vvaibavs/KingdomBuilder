1b6e3d12b66ec9e5a9698abe9fe311b19fd0ee86
purple57 <k1102391@students.katyisd.org>
2023-04-04 07:22:52 -0500

    empty inside

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -1,11 +1,29 @@
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 public class MainPanel extends JPanel {
     static Graphics graphic;
+    BufferedImage startScreen, endScreen, gameScreen;
     public MainPanel() {
         super();
         setSize(getWidth(),getHeight());
+        try {
+            startScreen = ImageIO.read(getClass().getResourceAsStream("/images/Start Screen.png"));
+            gameScreen = ImageIO.read(getClass().getResourceAsStream("/images/"));
+            endScreen = ImageIO.read(getClass().getResourceAsStream("/images/"));
+
+        }
+        catch(Exception e){
+
+            System.out.println(e.getMessage());
+            return;
+        }
     }
     public void paint(Graphics g){
         graphic = g;
