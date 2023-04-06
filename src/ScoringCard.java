cbf1a456dc4827517632803bd476c33638f244ca
purple57 <k1102391@students.katyisd.org>
2023-04-06 07:27:51 -0500

    scoring card

--- src/ScoringCard.java
+++ src/ScoringCard.java
@@ -1,6 +1,12 @@
+import java.awt.image.BufferedImage;
+
 public class ScoringCard {
     private String cardType;
+    private BufferedImage pic;
 
+    public ScoringCard(){
+
+    }
     public String getCardType() {
         return cardType;
     }
