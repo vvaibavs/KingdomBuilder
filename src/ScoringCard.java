6c4d87408e77f92e3edd6e84c50a29bb5da0f533
Vaibav <vaibavsubbu007@gmail.com>
2023-04-19 08:04:32 -0500

    scrong card

--- src/ScoringCard.java
+++ src/ScoringCard.java
@@ -5,6 +5,39 @@ public class ScoringCard {
     private BufferedImage pic;
 
     public ScoringCard(){
+        int random = (int)(Math.random() * 9) + 1;
+        switch(random) {
+            case 1:
+                cardType = "citizen";
+                break;
+            case 2:
+                cardType = "discoverer";
+                break;
+            case 3:
+                cardType = "farmer";
+                break;
+            case 4:
+                cardType = "fishermen";
+                break;
+            case 5:
+                cardType = "hermit";
+                break;
+            case 6:
+                cardType = "knight";
+                break;
+            case 7:
+                cardType = "lord";
+                break;
+            case 8:
+                cardType = "merchant";
+                break;
+            case 9:
+                cardType = "miner";
+                break;
+            case 10:
+                cardType = "worker";
+                break;
+        }
 
     }
     public String getCardType() {
