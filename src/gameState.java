f9b4b6053111228450e62c53a7c11a448ca22d87
spinklewink <ketchupeggs.beans@gmail.com>
2023-04-24 07:42:51 -0500

    scoring button

--- src/gameState.java
+++ src/gameState.java
@@ -19,15 +19,15 @@ public class gameState {
             state = "Game Screen";
             mouseX = 0;
             mouseY = 0;
-        } else if(mouseX > 476 && mouseY > 27 && mouseX < 734 && mouseY < 76 && !state.equals("Scoring Card") && !state.equals("not Scoring Card")) {
+        } else if(mouseX > 476 && mouseY > 27 && mouseX < 734 && mouseY < 76 && !state.equals("Scoring Card") && !state.equals("not Scoring Card")) { //scoring card
             state = "Scoring Card";
         } else if(mouseX > 476 && mouseY > 27 && mouseX < 734 && mouseY < 76 && state.equals("not Scoring Card")) {
             state = "Game Screen";
-        } else if(mouseX > 1342 && mouseY > 878 && mouseX < 1524 && mouseY < 926 && !state.equals("End Screen")) {
+        } else if(mouseX > 1342 && mouseY > 878 && mouseX < 1524 && mouseY < 926 && state.equals("Game Screen")) { //next button
             state = "End Screen";
         } else if(mouseX > 1342 && mouseY > 926 && mouseX < 1524 && mouseY < 926 && state.equals("End Screen")) {
             state = "Game Screen";
-        } else if(mouseX > 318 && mouseY > 761 && mouseX < 776 && mouseY < 828 && state.equals("End Screen")) {
+        } else if(mouseX > 318 && mouseY > 761 && mouseX < 776 && mouseY < 828 && state.equals("End Screen")) { //go to game board
             state = "Game Screen";
     }
 
