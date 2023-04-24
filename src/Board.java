53bb8618cbcedf99a9937fd0896bb558f43ab72b
Firebat1 <swinglemdavid@gmail.com>
2023-04-24 07:43:10 -0500

    scoring button

---
+++ src/Board.java
@@ -0,0 +1,14 @@
+public class Board {
+    public void Board(Sector sec1, Sector sec2, Sector sec3, Sector sec4) {
+
+    }
+}
+for (int i = 0; i < sector.length;  i ++) {
+        for (int j = 0; j < sector[i].length; j ++) {
+        if (i != sector.length - 1) {
+        if (i % 2 == 0) {
+        sector[i][j].addNeighbor("SouthEast", sector[i - 1][j]);
+        }
+        }
+        }
+        }
