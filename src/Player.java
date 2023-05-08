5ddc58d02d5d72f8be5d219a732f794032c05efd
PCSwingle <swingle8paul@gmail.com>
2023-05-07 23:17:43 -0500

    Empty Inside.

--- src/Player.java
+++ src/Player.java
@@ -15,7 +15,7 @@ public class Player {
     public TerrainCard card;
     public Player(String c){
         color = c;
-        numSettlements = 1;
+        numSettlements = 2;
         score = 0;
         tile = new ArrayList<LocTile>();
         turn = false;
